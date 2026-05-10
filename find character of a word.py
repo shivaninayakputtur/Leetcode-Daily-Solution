681d3e5019d9deccfc8cbd20f0e6251799c79bd0
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-05-10 21:01:12 +0530

    problem to find the character form the given words
    so in this problem we will enter a character for eg:"e" then the program will return the index of the given character in the words

---
+++ find character of a word.py
@@ -0,0 +1,10 @@
+def findWordsContaining(words,x):
+    result=[]
+    for i in range(len(words)):
+        if x in words[i]:
+            result.append(i)
+    return result
+words=["leet","code"]
+x=input("enter a character:")
+print(findWordsContaining(words,x))
+
