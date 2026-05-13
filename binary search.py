41eb541387d86fa1f4c6ea9c9a922043499786b9
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-05-13 20:45:04 +0530

    Problem on binary search
    In this problem a targeted value will be finding in the input

---
+++ binary search.py
@@ -0,0 +1,16 @@
+def searchInsert(nums,target):
+        left=0
+        right=len(nums)-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left=mid+1
+            else:
+                right=mid-1
+        return left
+nums=[1,3,4,5,7]
+target=5
+print(searchInsert(nums,target))
+            
