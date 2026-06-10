5f0bcaffdcd21b4b76ace1e2a3210119bda355d8
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-06-10 21:21:41 +0530

    Palindrome

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+def isPalindrome(x):        
+    if x < 0:
+        return False
+    original = x
+    reverse = 0
+    while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x //= 10
+    return original == reverse
+x=121
+print(isPalindrome(x))
